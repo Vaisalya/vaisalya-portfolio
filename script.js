6e7832caac67ae11486f74329499358fe90594c3
Vaisalya Damaroutu <damarouthuvaisalya@gmail.com>
2026-06-27 17:47:19 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,146 @@
+*{
+margin:0;
+padding:0;
+box-sizing:border-box;
+font-family:'Poppins',sans-serif;
+}
+
+body{
+
+background:#f8f9fa;
+
+color:#222;
+
+}
+
+header{
+
+display:flex;
+
+justify-content:space-between;
+
+align-items:center;
+
+padding:25px 8%;
+
+background:white;
+
+box-shadow:0 5px 15px rgba(0,0,0,.05);
+
+}
+
+header h2{
+
+color:#2563eb;
+
+}
+
+nav a{
+
+text-decoration:none;
+
+margin-left:30px;
+
+font-weight:600;
+
+color:#222;
+
+transition:.3s;
+
+}
+
+nav a:hover{
+
+color:#2563eb;
+
+}
+
+.home{
+
+display:flex;
+
+justify-content:space-between;
+
+align-items:center;
+
+padding:80px 8%;
+
+min-height:90vh;
+
+}
+
+.content{
+
+max-width:600px;
+
+}
+
+.content h1{
+
+font-size:55px;
+
+margin-bottom:15px;
+
+}
+
+.content span{
+
+color:#2563eb;
+
+}
+
+.content h3{
+
+font-size:28px;
+
+margin-bottom:20px;
+
+}
+
+.content p{
+
+font-size:17px;
+
+line-height:30px;
+
+color:#555;
+
+margin-bottom:35px;
+
+}
+
+.buttons a{
+
+display:inline-block;
+
+padding:13px 28px;
+
+background:#2563eb;
+
+color:white;
+
+text-decoration:none;
+
+border-radius:30px;
+
+margin-right:15px;
+
+transition:.3s;
+
+}
+
+.buttons a:hover{
+
+transform:translateY(-5px);
+
+}
+
+.image img{
+
+width:380px;
+
+border-radius:50%;
+
+box-shadow:0 20px 40px rgba(0,0,0,.2);
+
+}
